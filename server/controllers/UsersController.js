3316d6092e7324b34307cb5240973334fa724398
Andela Developer <omobosteven@gmail.com>
2018-05-24 10:58:42 +0100

    ft(signin-user): signin user endpoint 	add login method on the UserController.js file 	add the login route on the users.js file 	write tests for the login method 	update readme [finishes #157781693]

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -63,6 +63,61 @@ class UsersController extends Controller {
           });
       });
   }
+
+  /**
+   * @description Method to login a new user
+   *
+   * @param {Object} req - HTTP Request
+   * @param {Object} res - HTTP Response
+   *
+   * @return {Object} Returned object
+   */
+  static login(req, res) {
+    const { body } = req;
+
+    const emailAddress = body.email.trim().toLowerCase();
+    const queryGetUser = `SELECT userId, email, role, password
+                          FROM users
+                          WHERE email = '${emailAddress}'`;
+
+    db.connect()
+      .then((client) => {
+        client.query(queryGetUser)
+          .then((user) => {
+            if (!user.rows[0]) {
+              return res.status(404).json({
+                status: 'fail',
+                message: 'User not found',
+              });
+            }
+
+            const checkPassword = bcrypt
+              .compareSync(body.password.trim(), user.rows[0].password);
+            if (!checkPassword) {
+              return res.status(400).json({
+                status: 'fail',
+                message: 'Wrong password',
+              });
+            }
+
+            const authToken = Util.token(user.rows[0]);
+            return res.status(200).json({
+              status: 'success',
+              message: 'Sign in successfully',
+              data: {
+                token: authToken,
+              },
+            });
+          })
+          .catch(() => {
+            client.release();
+            return res.status(500).json({
+              status: 'error',
+              message: 'internal server error',
+            });
+          });
+      });
+  }
 }
 
 export default UsersController;
