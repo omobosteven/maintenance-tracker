18233d075ab8e2472d073350563ebf0749959e07
omobosteven <omobosteven@gmail.com>
2018-05-29 15:18:30 +0100

    ft(disapprove-request): endpoint to disapprove a request -	add the route to routes/requests.js -	write test for the endpoint on tests/adminRequestController.spec.js -	update readme [finishes #157903304]

--- server/tests/adminRequestController.spec.js
+++ server/tests/adminRequestController.spec.js
@@ -24,6 +24,26 @@ describe('Tests for admin requests API endpoints', () => {
       });
   });
 
+  it('should create request', (done) => {
+    chai.request(app)
+      .post('/api/v1/users/requests')
+      .set('x-access-token', userToken)
+      .set('Content-type', 'application/json')
+      .send({
+        type: 'maintenance',
+        category: 'computers',
+        item: 'laptop',
+        description: 'faulty screen',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.message).to.equal('request created successfully');
+        expect(res.body.data.request.item).to.equal('laptop');
+        expect(res.body.data.request.description).to.equal('faulty screen');
+        done();
+      });
+  });
+
   it('should fetch all the requests in the system', (done) => {
     chai.request(app)
       .get('/api/v1/requests')
@@ -50,6 +70,30 @@ describe('Tests for admin requests API endpoints', () => {
         expect(res).to.have.status(200);
         expect(res.body.message).to.equal('Request approved');
         expect(res.body.data.request.status).to.equal('approved');
+        done();
+      });
+  });
+
+  it('should disapprove a request', (done) => {
+    chai.request(app)
+      .put('/api/v1/requests/2/disapprove')
+      .set('x-access-token', userToken)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Request disapproved');
+        expect(res.body.data.request.status).to.equal('disapproved');
+        done();
+      });
+  });
+
+  it('should fail if admin tries to disapporve an approved request', (done) => {
+    chai.request(app)
+      .put('/api/v1/requests/1/disapprove')
+      .set('x-access-token', userToken)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.status).to.equal('fail');
+        expect(res.body.message).to.equal('Request was not found');
         done();
       });
   });
